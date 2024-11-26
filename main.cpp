57a4d3e946abf2e0a60d7fbb4adb90b65e96e62e
fontflows <eduardofonteneleifce@gmail.com>
2024-11-25 22:34:40 -0300

    fixing issues with the main

--- main.cpp
+++ main.cpp
@@ -54,7 +54,67 @@ int main() {
     cout << "Escolha uma opcao: ";
     cin >> choice;
 
-    if (choice == 2) {
+    if (choice == 1) {
+        int n;
+        cout << "Digite o tamanho do vetor: ";
+        cin >> n;
+
+        int* arr = new int[n + 1];
+        cout << "Digite os " << n << " elementos do vetor:\n";
+        
+        cin.ignore();
+        
+        for (int i = 1; i <= n; i++) {
+            cout << "Elemento " << i << ": ";
+            cin >> arr[i];
+        }
+
+        cout << "\nVetor original: ";
+        utils::printArray(arr, n);
+        cout << "\nResultados das ordenacoes:\n\n";
+
+        int* temp = new int[n + 1];
+
+        cout << "Insercao Direta: ";
+        utils::copyArray(arr, temp, n);
+        sorting::insertionSort(temp, n);
+        utils::printArray(temp, n);
+
+        cout << "Insercao Binaria: ";
+        utils::copyArray(arr, temp, n);
+        sorting::binaryInsertionSort(temp, n);
+        utils::printArray(temp, n);
+
+        cout << "Selecao: ";
+        utils::copyArray(arr, temp, n);
+        sorting::selectionSort(temp, n);
+        utils::printArray(temp, n);
+
+        cout << "Bubblesort: ";
+        utils::copyArray(arr, temp, n);
+        sorting::bubbleSort(temp, n);
+        utils::printArray(temp, n);
+
+        cout << "Heapsort: ";
+        utils::copyArray(arr, temp, n);
+        sorting::heapSort(temp, n);
+        utils::printArray(temp, n);
+
+        cout << "Mergesort: ";
+        utils::copyArray(arr, temp, n);
+        sorting::mergeSort(temp, n);
+        utils::printArray(temp, n);
+
+        cout << "Quicksort: ";
+        utils::copyArray(arr, temp, n);
+        sorting::quickSort(temp, 1, n);
+        utils::printArray(temp, n);
+
+        delete[] arr;
+        delete[] temp;
+    }
+
+    else if (choice == 2) {
         filesystem::create_directory("../data");
         vector<string> fileNames = {
             "../data/ascending_resultados.csv",
@@ -68,6 +128,7 @@ int main() {
         }
 
         vector<int> sizes = {1000, 5000, 10000, 50000, 100000};
+        //vector<int> sizes = {10, 20, 30, 40, 50};
         vector<string> arrayTypes = {"ascending", "descending", "random"};
 
         for (const auto& type : arrayTypes) {
@@ -84,5 +145,6 @@ int main() {
         cout << "\nTodos os testes foram concluidos!\n";
         cout << "Os resultados foram exportados para os arquivos 'ascending_resultados.csv', 'descending_resultados.csv' e 'random_resultados.csv'.\n";
     }
+
     return 0;
 }
